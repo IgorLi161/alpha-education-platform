72107c2c59a0fa959f91b1e053b1bfb394113782
IgorLi161 <leeigor_a@mail.ru>
2025-06-18 00:29:22 +0500

    fix: vite config for render deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,14 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
 
 export default defineConfig({
+  base: './',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-  },
-  server: { port: 5173 }
+  }
 })
